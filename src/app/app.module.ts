551d3f1b274b892efaaf63fa8518732bb314dfb0
Jefferson-web <jeffer_16_99@hotmail.com>
2020-02-06 09:42:58 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { PagesModule } from './pages/pages.module';
+/* Routes */
+import { APP_ROUTES } from './app-routes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
